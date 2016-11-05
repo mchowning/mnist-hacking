9656ad7caf56b7b2e8e740c72cb1c2a39ec23636
Matt Chowning <mchowning@gmail.com>
2016-11-04 22:45:29 -0400

    Very rough, but seemingly working, backpropogation

--- scala/src/main/scala/CostFunc.scala
+++ scala/src/main/scala/CostFunc.scala
@@ -2,14 +2,13 @@ import breeze.linalg.{DenseVector, sum}
 
 trait CostFunc {
   def apply(expected: DenseVector[Double], actual: DenseVector[Double]): Double
-  // TODO should this be using vectors?
-  def derivative(expected: Double, actual: Double): Double
+  def derivative(expected: DenseVector[Double], actual: DenseVector[Double]): DenseVector[Double]
 }
 
 object ResidualSumOfSquares extends CostFunc {
   override def apply(expected: DenseVector[Double], actual: DenseVector[Double]): Double =
     sum((expected - actual) :^ 2d) / 2
 
-  override def derivative(expected: Double, actual: Double): Double =
+  override def derivative(expected: DenseVector[Double], actual: DenseVector[Double]): DenseVector[Double] =
     actual - expected
 }
--- scala/src/main/scala/NeuralNet.scala
+++ scala/src/main/scala/NeuralNet.scala
@@ -1,7 +1,7 @@
-import breeze.linalg.{DenseMatrix, DenseVector}
+import breeze.linalg.{*, DenseMatrix, DenseVector}
 
 // TODO use Streams
-// TODO use 3d matrix instead of list of 2d matrices for weights
+// TODO use 3d matrix instead of list of 2d matrices for weights?
 
 /**
   * @param userWeights each DenseMatrix represents the weights going from a particular node in
@@ -13,14 +13,14 @@ class NeuralNet(numNodesPerLayer: List[Int],
                 activationFunc: ActivationFunc = NeuralNet.DefaultActivationFunc,
                 costFunc: CostFunc = NeuralNet.DefaultCostFunc) {
 
-  val weights = if (userWeights != Nil) userWeights else randomWeights()
+  val weights: List[DenseMatrix[Double]] = if (userWeights != Nil) userWeights else randomWeights()
 
   def randomWeights() = {
     println("Initializing with random weights...")
     // List of tuples where each tuple represents (# input nodes, # output nodes) from one layer to the next
     // adding one node to first layer to get weights for bias node (not adding to layer2 because bias nodes take no input)
     numNodesPerLayer.zip(numNodesPerLayer.tail)
-      .map { t => DenseMatrix.rand(t._1 + 1, t._2) }
+      .map { t => DenseMatrix.rand(t._1 + 1, t._2): DenseMatrix[Double] }
 //      .map { t => DenseMatrix.rand(t._1, t._2, new RandBasis(new ThreadLocalRandomGenerator(new MersenneTwister(0))).uniform) }
   }
 
@@ -30,19 +30,73 @@ class NeuralNet(numNodesPerLayer: List[Int],
   }
 
   def train(inputs: List[Double], expected: DenseVector[Double]) = {
-    val output: DenseVector[Double] = forwardPropagate(inputs)
+
+    val activations: List[DenseVector[Double]] = forwardPropActivations(inputs)
+    println(s"activations: $activations")
+    val output: DenseVector[Double] = activations.last
     val cost = costFunc(expected, output)
-    // TODO backprop
+
+    val dcda: DenseVector[Double] = costFunc.derivative(expected, output)
+    println(s"dcda: $dcda")
+//    val dadz = output.map(activationFunc.derivative)
+//    println(s"dadz: $dadz")
+
+    // Adding matrix of identityWeights for "weights" between final node(s) and error calcs
+    // TODO why do I need to declare the type here?
+    val modifiedWeights: List[DenseMatrix[Double]] = weights :+ DenseMatrix.eye[Double](weights.last.cols)
+
+    val weightActivationPairs: List[(DenseMatrix[Double], DenseVector[Double])] = modifiedWeights.reverse zip activations.reverse
+    println(s"weightActivationPairs: $weightActivationPairs")
+
+    // TODO try scanRight without reversing?
+    // derivative of total error with respect to pre-neuronal activation
+    val nodeErrors: List[DenseVector[Double]] = weightActivationPairs.scanLeft(dcda) {
+      case (upperNodeError: DenseVector[Double], (weightMatrix, activationsBelow)) => {
+        {
+          {
+//            println(s"upperNodeError: $upperNodeError")
+//            println(s"weightMatrix: $weightMatrix")
+//            println(s"activationsBelow: $activationsBelow")
+            val deda: DenseVector[Double] = weightMatrix * upperNodeError
+            println(s"deda: $deda")
+            val dadz: DenseVector[Double] = activationsBelow.map(activationFunc.derivative)
+            println(s"dadz: $dadz")
+            dadz :* deda
+          }
+        }
+      }
+    }.tail // drop the initial (reversed) term which only relates to the cost function level
+
+    println(s"nodeErrors: $nodeErrors")
+
+    val dedw: List[DenseVector[Double]] = nodeErrors.init zip activations.init.reverse map { case (errors, acts) =>
+      errors :* acts
+    }
+    println(s"dedw: $dedw")
+
+    val learningRate: Double = 0.5
+    println(s"weights.reverse: ${weights.reverse}")
+
+
+    val newWeights = weights.reverse zip dedw map { case (ws, ds) =>
+      ws(*,::) - (ds * learningRate)
+    }
+    println(s"newWeights: $newWeights")
+
   }
 
-  def forwardPropagate(inputs: List[Double]): DenseVector[Double] = {
+  private def forwardPropActivations(inputs: List[Double]): List[DenseVector[Double]] = {
     val inputVals = DenseVector(inputs.toArray)
-    weights.foldLeft(inputVals) { (iVals, wVals) =>
+    weights.scanLeft(inputVals) { (iVals, wVals) =>
       // TODO find better way to add to a vector
       val inputValsWithBias = DenseVector(iVals.toArray :+ 1d)
       (wVals.t * inputValsWithBias)  // multiply weights and inputs
         .map(activationFunc.apply)   // run results through activation function
     }
+  }
+
+  def forwardPropagate(inputs: List[Double]): DenseVector[Double] = {
+    forwardPropActivations(inputs).last
   }
 }
 
