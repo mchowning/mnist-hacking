d57c113a43533df8d1e0c9e0abdb119e1d044a40
Matt Chowning <mchowning@gmail.com>
2016-11-05 10:17:50 -0400

    Adding initial backprop test

--- scala/src/main/scala/NeuralNet.scala
+++ scala/src/main/scala/NeuralNet.scala
@@ -10,8 +10,9 @@ import breeze.linalg.{*, DenseMatrix, DenseVector}
 // TODO fix this constructor, don't need both numNodesPerLayer and userWeights
 class NeuralNet(numNodesPerLayer: List[Int],
                 userWeights: List[DenseMatrix[Double]] = Nil,
-                activationFunc: ActivationFunc = NeuralNet.DefaultActivationFunc,
-                costFunc: CostFunc = NeuralNet.DefaultCostFunc) {
+                activationFunc: ActivationFunc = Sigmoid,
+                costFunc: CostFunc = ResidualSumOfSquares,
+                learningRate: Double = 0.5) {
 
   val weights: List[DenseMatrix[Double]] = if (userWeights != Nil) userWeights else randomWeights()
 
@@ -21,7 +22,7 @@ class NeuralNet(numNodesPerLayer: List[Int],
     // adding one node to first layer to get weights for bias node (not adding to layer2 because bias nodes take no input)
     numNodesPerLayer.zip(numNodesPerLayer.tail)
       .map { t => DenseMatrix.rand(t._1 + 1, t._2): DenseMatrix[Double] }
-//      .map { t => DenseMatrix.rand(t._1, t._2, new RandBasis(new ThreadLocalRandomGenerator(new MersenneTwister(0))).uniform) }
+      // .map { t => DenseMatrix.rand(t._1, t._2, new RandBasis(new ThreadLocalRandomGenerator(new MersenneTwister(0))).uniform) }
   }
 
   def totalError(inputs: List[Double], expected: DenseVector[Double]): Double = {
@@ -29,24 +30,19 @@ class NeuralNet(numNodesPerLayer: List[Int],
     costFunc(expected, output)
   }
 
-  def train(inputs: List[Double], expected: DenseVector[Double]) = {
+  def train(inputs: List[Double], expected: DenseVector[Double]): List[DenseMatrix[Double]] = {
 
     val activations: List[DenseVector[Double]] = forwardPropActivations(inputs)
-    println(s"activations: $activations")
     val output: DenseVector[Double] = activations.last
     val cost = costFunc(expected, output)
 
     val dcda: DenseVector[Double] = costFunc.derivative(expected, output)
-    println(s"dcda: $dcda")
-//    val dadz = output.map(activationFunc.derivative)
-//    println(s"dadz: $dadz")
 
     // Adding matrix of identityWeights for "weights" between final node(s) and error calcs
     // TODO why do I need to declare the type here?
     val modifiedWeights: List[DenseMatrix[Double]] = weights :+ DenseMatrix.eye[Double](weights.last.cols)
 
     val weightActivationPairs: List[(DenseMatrix[Double], DenseVector[Double])] = modifiedWeights.reverse zip activations.reverse
-    println(s"weightActivationPairs: $weightActivationPairs")
 
     // TODO try scanRight without reversing?
     // derivative of total error with respect to pre-neuronal activation
@@ -54,35 +50,21 @@ class NeuralNet(numNodesPerLayer: List[Int],
       case (upperNodeError: DenseVector[Double], (weightMatrix, activationsBelow)) => {
         {
           {
-//            println(s"upperNodeError: $upperNodeError")
-//            println(s"weightMatrix: $weightMatrix")
-//            println(s"activationsBelow: $activationsBelow")
             val deda: DenseVector[Double] = weightMatrix * upperNodeError
-            println(s"deda: $deda")
             val dadz: DenseVector[Double] = activationsBelow.map(activationFunc.derivative)
-            println(s"dadz: $dadz")
             dadz :* deda
           }
         }
       }
     }.tail // drop the initial (reversed) term which only relates to the cost function level
 
-    println(s"nodeErrors: $nodeErrors")
-
     val dedw: List[DenseVector[Double]] = nodeErrors.init zip activations.init.reverse map { case (errors, acts) =>
       errors :* acts
     }
-    println(s"dedw: $dedw")
 
-    val learningRate: Double = 0.5
-    println(s"weights.reverse: ${weights.reverse}")
-
-
-    val newWeights = weights.reverse zip dedw map { case (ws, ds) =>
+    (weights.reverse zip dedw).map { case (ws, ds) =>
       ws(*,::) - (ds * learningRate)
-    }
-    println(s"newWeights: $newWeights")
-
+    }.reverse
   }
 
   private def forwardPropActivations(inputs: List[Double]): List[DenseVector[Double]] = {
@@ -101,6 +83,5 @@ class NeuralNet(numNodesPerLayer: List[Int],
 }
 
 object NeuralNet {
-  val DefaultActivationFunc: ActivationFunc = Sigmoid
-  val DefaultCostFunc: CostFunc = ResidualSumOfSquares
+  val LearningRate: Double = 0.5
 }
--- scala/src/test/scala/NeuralNetSpec.scala
+++ scala/src/test/scala/NeuralNetSpec.scala
@@ -9,16 +9,29 @@ class NeuralNetSpec extends FreeSpec with Matchers {
                             userWeights = List(DenseMatrix((0.15, 0.25), (0.20, 0.30), (0.35, 0.35)),
                                                 DenseMatrix((0.40, 0.50), (0.45, 0.55), (0.6, 0.6))),
                             activationFunc = Sigmoid,
-                            costFunc = ResidualSumOfSquares)
+                            costFunc = ResidualSumOfSquares,
+                            learningRate = 0.5)
+    val inputs: List[Double] = List(0.05, 0.10)
+    val expected: DenseVector[Double] = DenseVector(0.01, 0.99)
 
     "should forward propagate" in {
-      val outputs = nn.forwardPropagate(List(0.05, 0.10))
+      val outputs = nn.forwardPropagate(inputs)
       outputs shouldEqual DenseVector(0.7513650695523157, 0.7729284653214625)
     }
 
     "should calculate total error" in {
-      val error = nn.totalError(List(0.05, 0.10), DenseVector(0.01, 0.99))
+      val error = nn.totalError(inputs, expected)
       error shouldEqual 0.2983711087600027
+    }
+
+    "should backpropagate the weights" in {
+      val newWeights = nn.train(inputs, expected)
+      newWeights shouldEqual List(DenseMatrix((0.1497807161327628,  0.24950228726473914),
+                                              (0.19978071613276283, 0.29950228726473915),
+                                              (0.3497807161327628,  0.34950228726473914)),
+                                  DenseMatrix((0.35891647971788465, 0.5113701211079891 ),
+                                              (0.40891647971788464, 0.5613701211079891 ),
+                                              (0.5589164797178846,  0.611370121107989  )))
     }
   }
 }
